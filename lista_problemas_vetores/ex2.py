e9bba85f63fcbed9ce072d637338bf85c621eeeb
ghalves3701 <ghcarvalhoalves@gmail.com>
2025-11-18 21:54:55 -0300

    ex2 e 3 feitos

--- lista_problemas_vetores/ex2.py
+++ lista_problemas_vetores/ex2.py
@@ -3,3 +3,13 @@
 - Novo For para vasculhar o vetor preenchido com todas as suas posições
 - Quando o valor na posição do índice do vetor for igual ao valor da variável procurar, apresente a mensagem: 'Valor {x} encontrado na posição {i} de índice do vetor’
 - Ao final, apresente quantos valores foram encontrados.'''
+vetor=[]
+cont=0
+for i in range (14):
+    vetor.append(int(input(f'Digite o {i+1}º valor: ')))
+procurar=int(input('Entre com um valor para localizar: '))
+for i in range(len(vetor)):
+    if procurar==(vetor[i]):
+        cont+=1
+        print(f'Valor {procurar} encontrado na posição: {i} ')
+print(f'Quantidade de valores encontrados: {cont}')
--- lista_problemas_vetores/ex3.py
+++ lista_problemas_vetores/ex3.py
@@ -2,3 +2,11 @@
 - A def deverá receber uma letra via input, digitada pelo usuário
 - Caso a letra digitada não esteja entre a, e, i, o e u, apresente a mensagem: 'Letra Inválida! ' e, depois, reexecute a def vogal() chamando-a novamente e recomeçando o processo.
 - Sendo uma letra vogal digitada entre a, e, i, o e u, digite a mensagem 'Letra ok: {letra} '''
+def vogal():
+    letra=str(input("Entre com uma letra: ")).upper()
+    if letra in ('A', 'E', 'I', 'O', 'U'):
+        print(f'Letra ok: {letra}')
+    else:
+        print('\n Letra inválida \n')
+        vogal()
+vogal()   
